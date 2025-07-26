76450eaa5b371c06be4a67715286e7f8ac8cab21
Shubhranshu <shubhranshumohandas01062000@gmail.com>
2025-07-26 22:01:48 +0530

    Initial commit

---
+++ Frontend/Angular/flight-hunter/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { SearchFlightsComponent } from './search-flights/search-flights.component';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+  {path: 'search-flight', component: SearchFlightsComponent}
+];
---
+++ Frontend/Angular/flight-hunter/src/app/search-flights/search-flights.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-flights',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-flights.component.html',
+  styleUrl: './search-flights.component.css'
+})
+export class SearchFlightsComponent {
+  flights: FlightDeailInterface[] = [
+    {airline: 'Air India', arrival: {time: Date.now().toString(), place: 'New York'}, departure: {time: Date.now().toString(), place: 'Los Angeles'}, remainingSeats: 50, price: 300},
+    {airline: 'Delta Airlines', arrival: {time: Date.now().toString(), place: 'Chicago'}, departure: {time: Date.now().toString(), place: 'Los Angeles'}, remainingSeats: 30, price: 250},
+    {airline: 'United Airlines', arrival: {time: Date.now().toString(), place: 'San Francisco'}, departure: {time: Date.now().toString(), place: 'Los Angeles'}, remainingSeats: 20, price: 400},
+    {airline: 'Southwest Airlines', arrival: {time: Date.now().toString(), place: 'Miami'}, departure: {time: Date.now().toString(), place: 'Los Angeles'}, remainingSeats: 15, price: 350},
+    {airline: 'JetBlue Airways', arrival: {time: Date.now().toString(), place: 'Boston'}, departure: {time: Date.now().toString(), place: 'Los Angeles'}, remainingSeats: 25, price: 280}
+  ];
+}
+
+export interface FlightDeailInterface {
+  airline: string;
+  arrival: TimePlaceInterface;
+  departure: TimePlaceInterface;
+  remainingSeats: number;
+  price: number;
+}
+
+export interface TimePlaceInterface {
+  time: string;
+  place: string;
+}
